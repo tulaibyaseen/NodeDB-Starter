1af009bf4b038068eec12da3ea71e145cb7d4c7c
hamid2117 <saim008865@gmail.com>
2025-03-03 07:55:39 +0500

    feat: add migration for creating Users table with necessary fields

---
+++ migrations/20250303015013-create-user.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.literal('uuid_generate_v4()'),
+        primaryKey: true,
+      },
+      name: { type: Sequelize.STRING, allowNull: false },
+      email: { type: Sequelize.STRING, allowNull: false, unique: true },
+      passwordHash: { type: Sequelize.STRING, allowNull: false },
+      isVerified: { type: Sequelize.BOOLEAN, defaultValue: false },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    })
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('Users')
+  },
+}
